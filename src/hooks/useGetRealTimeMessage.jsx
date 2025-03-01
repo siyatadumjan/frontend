520b54e20d36c34a99e58710e947f69140a38e1c
siyatadumjan <135720118+siyatadumjan@users.noreply.github.com>
2025-03-01 16:40:36 +0545

    realtime

--- src/hooks/useGetRealTimeMessage.jsx
+++ src/hooks/useGetRealTimeMessage.jsx
@@ -3,7 +3,7 @@ import {useSelector, useDispatch} from "react-redux";
 import { setMessages } from "../redux/messageSlice";
 
 const useGetRealTimeMessage = () => {
-    const {socket} = useSelector(store=>store.socket);
+    const {socke} = useSelector(store=>store.socket);
     const {messages} = useSelector(store=>store.message);
     const dispatch = useDispatch();
     useEffect(()=>{
